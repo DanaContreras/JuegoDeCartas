ed0f0ff43eeaf8d589d1c490323449d4d915fb37
Gaston <gaston.verussa@est.fi.uncoma.edu.ar>
2021-09-14 14:05:02 -0300

    Ultimo commit no subió las últimas modificaciones a HiloCallable, ahora si:
    Clase HiloCallable lista, falta escribir un ejemplo en el MainDePrueba 
    para testearla.

--- TrabajoPractico1/src/trabajopractico1/HiloCallable.java
+++ TrabajoPractico1/src/trabajopractico1/HiloCallable.java
@@ -2,18 +2,79 @@ package trabajopractico1;
 import java.util.concurrent.Callable;
 import java.util.Random;
 
-public class HiloCallable implements Callable{
+public class HiloCallable implements Callable<Carta>{
     
+    //Carta result que guarda el resultado que se pasará al Future
     private final Carta result = new Carta();
     private final Random random = new Random();
+    
+    //Variable para llevar el control
+    private final String nombre;
+    private final String color;
+    TextoAColor textoConColor = new TextoAColor();
+    
+    public HiloCallable(int numero, String color){
+        this.nombre = "Hilo callable " + numero;
+        this.color = color;
+        System.out.println(textoConColor.cambiarColor(color, nombre + ": Hilo creado"));
+    }
     
     @Override
     public Carta call(){
         
+        System.out.println(textoConColor.cambiarColor(color, nombre + ": Comienzo creación de carta"));
+        
+        //Objetos utilizados mas adelante
+        Personaje personaje;
+        PersonajeBuilder constructorPersonaje;
+        Director director = new Director();
+        
+        //Decide al azar que tipo de personaje tendrá la carta
+        switch(random.nextInt(3)){
+            case 0:
+                constructorPersonaje = new PersonajeFuego();
+                break;
+            case 1:
+                constructorPersonaje = new PersonajeNieve();
+                break;
+            default:
+                constructorPersonaje = new PersonajeAgua();
+        }
+        
+        //Decide al azar que nivel de personaje tendrá la carta, utilizando el
+        // director, pasandole el constructor seleccionado anteriormente
+        switch(random.nextInt(3)){
+            case 0:
+                personaje = director.construirPersonajeNivel1(constructorPersonaje);
+                break;
+            case 1:
+                personaje = director.construirPersonajeNivel2(constructorPersonaje);
+                break;
+            default:
+                personaje = director.construirPersonajeNivel3(constructorPersonaje);
+        }
+        
+        //Una vez que tiene el personaje, crea una carta de un tipo al azar con 
+        // el personaje
+        switch(random.nextInt(3)){
+            case 0:
+                construirCartaFuego(personaje);
+                break;
+            case 1:
+                construirCartaNieve(personaje);
+                break;
+            default:
+                construirCartaAgua(personaje);
+        }
+                
+        System.out.println(textoConColor.cambiarColor(color, nombre + ": Finalizo creación de carta"));
         
         return result;
     }
     
+    //Metodos hechos aparte para aislar la construccion de la carta
+    
+    //Metodo para construir una carta de fuego
     public void construirCartaFuego(Personaje personaje){
         
         //Arreglos para elegir al azar entres las opciones de valores o colores posibles
@@ -29,12 +90,19 @@ public class HiloCallable implements Callable{
         this.result.setColor(posiblesColores[random.nextInt(posiblesColores.length)]);
         
         this.result.setPersonaje(personaje);
-        this.result.setDescripcion("Carta de Fuego: " +
+        
+        //Descripcion, la parte de la carta tiene color segun su tipo y la de 
+        // personaje mantiene su color original
+        this.result.setDescripcion(
+            textoConColor.cambiarColor("Rojo", "Carta de Fuego: " +
                 "\n- Su valor es: " + this.result.getValor() + 
                 "\n- Es de Color " + this.result.getColor() + 
-                "\n- Su personaje es: " + personaje.getDescripcion());
+                "\n- ")
+            + personaje.toString());
     }
     
+    
+    //Metodo para construir una carta de nieve
     public void construirCartaNieve(Personaje personaje){
         
         //Arreglos para elegir al azar entres las opciones de valores posibles para
@@ -49,12 +117,19 @@ public class HiloCallable implements Callable{
         
         this.result.setColor("Transparente");
         this.result.setPersonaje(personaje);
-        this.result.setDescripcion("Carta de Nieve: " +
+        
+        //Descripcion, la parte de la carta tiene color segun su tipo y la de 
+        // personaje mantiene su color original
+        this.result.setDescripcion(
+            textoConColor.cambiarColor("Blanco", "Carta de Nieve: " +
                 "\n- Su valor es: " + this.result.getValor() + 
                 "\n- Es Transparente " +
-                "\n- Su personaje es: " + personaje.getDescripcion());
+                "\n- ")
+            + personaje.toString());
     }
     
+    
+    //Metodo para construir una carta de Agua
     public void construirCartaAgua(Personaje personaje){
         
         //Arreglos para elegir al azar entres las opciones de valores o colores posibles
@@ -70,9 +145,14 @@ public class HiloCallable implements Callable{
         this.result.setColor(posiblesColores[random.nextInt(posiblesColores.length)]);
         
         this.result.setPersonaje(personaje);
-        this.result.setDescripcion("Carta de AGUA: " +
+        
+        //Descripcion, la parte de la carta tiene color segun su tipo y la de 
+        // personaje mantiene su color original
+        this.result.setDescripcion(
+            textoConColor.cambiarColor("Azul", "Carta de AGUA: " +
                 "\n- Su valor es: " + this.result.getValor() + 
                 "\n- Es de Color " + this.result.getColor() + 
-                "\n- Su personaje es: " + personaje.getDescripcion());
+                "\n- ")
+             + personaje.toString());
     }
 }
